9cdcd5e45c7aa484821ae1001fec708c75c0da93
Lucas Montano <lucasmontano@gmail.com>
2020-05-12 18:50:31 +0200

    defining request payload for voting

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
+
+
+class Vote(BaseModel):
+    identifier: str
+    options: List[str]
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/vote/")
+def vote(vote: Vote):
+    return vote
