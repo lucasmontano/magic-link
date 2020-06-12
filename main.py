0d6300e0d16ba1c49723bc92c80081eb69f4d3c5
Lucas Gabriel <48874910+LucasGabrielB@users.noreply.github.com>
2020-06-12 02:17:55 -0300

    code refactoring
    Changed all string concatenations to f-strings.
    All double quotes changed to single quotes, to keep pattern.
    Added spaces for organization.

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-INVALID_IDENTIFIER = "Please, check identifier."
-INVALID_JWT = "Of course! You shall not pass!"
+INVALID_IDENTIFIER = 'Please, check identifier.'
+INVALID_JWT = 'Of course! You shall not pass!'
 
 JWT_SECRET = os.environ.get('JWT_SECRET')
 
@@ -22,7 +22,6 @@ app = FastAPI()
 redis_conn = Redis()
 magic_link_queue = Queue('send_magic_links', connection=redis_conn)
 
-
 class MagicLink(BaseModel):
     identifier: EmailStr
     payload: dict
@@ -30,26 +29,30 @@ class MagicLink(BaseModel):
 
 def confirm_identifier(magic: MagicLink):
     encoded = jwt.encode(jsonable_encoder(magic.dict()), JWT_SECRET, algorithm='HS256').decode('utf-8')
-    print("Payload enconded to JWT: " + encoded)
-    print("sending confirmation to: " + magic.identifier)
+    print(f'Payload enconded to JWT: {encoded}')
+    print(f'sending confirmation to: {magic.identifier}')
+    
     message = Mail(
-        from_email=os.environ.get('SENDGRID_SENDER'),
-        to_emails=magic.identifier,
-        subject='Are you a bot?',
-        html_content='<a href="http://127.0.0.1:8000/validate/?token=' + encoded + '">Confirm you are human or a super smart bot: </a>')
+        from_email = os.environ.get('SENDGRID_SENDER'),
+        to_emails = magic.identifier,
+        subject = 'Are you a bot?',
+        html_content = f'<a href="http://127.0.0.1:8000/validate/?token={encoded}">Confirm you are human or a super smart bot: </a>'
+    )
+    
     try:
         sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
         response = sg.send(message)
         print(response.status_code)
         print(response.body)
         print(response.headers)
+    
     except Exception as e:
         print(e.body)
 
     return magic.payload
 
 
-@app.get("/validate/", status_code=204)
+@app.get('/validate/', status_code=204)
 def validate(token: str):    
     try:        
         magic_link = parse_obj_as(
@@ -57,20 +60,22 @@ def validate(token: str):
             jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
         )
 
-        cred = credentials.Certificate("./serviceAccountKey.json")
+        cred = credentials.Certificate('./serviceAccountKey.json')
         firebase_admin.initialize_app(cred)
         db = firestore.client()
 
         doc_ref = db.collection('users').document(magic_link.identifier)
         doc_ref.set(magic_link.payload)
+    
     except Exception as e:
         print(e)
         error_detail = jsonable_encoder({
-            "message": INVALID_JWT
+            'message': INVALID_JWT
         })
+        
         raise HTTPException(status_code=400, detail=error_detail)
         
 
-@app.post("/send/", status_code=204)
+@app.post('/send/', status_code=204)
 def send(magic: MagicLink):
     magic_link_queue.enqueue(confirm_identifier, magic)
